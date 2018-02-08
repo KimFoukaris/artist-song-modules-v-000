cbfa638e25d489ca31ec0cd30a24f5979a4257ca
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-08 16:00:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods
-
-  extend Findable
   include Memorable::InstanceMethods
+  extend Findable
   include Paramable
 
   attr_accessor :name
